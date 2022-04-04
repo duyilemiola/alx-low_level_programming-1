ffccf564af66b8fdec40ee870bfc11d54ddbbe27
AdePelumi <lordbatech00@gmail.com>
2022-04-04 08:59:26 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,68 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
- *_strdup - returns a pointer to a newly allocated space
- *@str: pointer
- *Return: NULL if str=0
- */
-
-char *_strdup(char *str)
+* count_word - helper function to count the number of words in a string
+* @s: string to evaluate
+*
+* Return: number of words
+*/
+int count_word(char *s)
 {
-int i, size;
-char *a;
-if (str == NULL)
-return (NULL);
-
-for (size = 0; str[size] != '\0'; size++)
+int flag, c, w;
+flag = 0;
+w = 0;
+for (c = 0; s[c] != '\0'; c++)
 {
-a = malloc(size * sizeof(*str) + 1);
-/** +1 on size puts the end of string car*/
-if (a == 0)
-return (NULL);
-else
+if (s[c] == ' ')
+flag = 0;
+else if (flag == 0)
 {
-for (i = 0; i <= size; i++)
-a[i] = str[i];
+flag = 1;
+w++;
 }
 }
-return (a);
+return (w);
+}
+/**
+* **strtow - splits a string into words
+* @str: string to split
+*
+* Return: pointer to an array of strings (Success)
+* or NULL (Error)
+*/
+char **strtow(char *str)
+{
+char **matrix, *tmp;
+int i, k = 0, len = 0, words, c = 0, start, end;
+while (*(str + len))
+len++;
+words = count_word(str);
+if (words == 0)
+return (NULL);
+matrix = (char **) malloc(sizeof(char *) * (words + 1));
+if (matrix == NULL)
+return (NULL);
+for (i = 0; i <= len; i++)
+{
+if (str[i] == ' ' || str[i] == '\0')
+{
+if (c)
+{
+end = i;
+tmp = (char *) malloc(sizeof(char) * (c + 1));
+if (tmp == NULL)
+return (NULL);
+while (start < end)
+*tmp++ = str[start++];
+*tmp = '\0';
+matrix[k] = tmp - c;
+k++;
+c = 0;
+}
+}
+else if (c++ == 0)
+start = i;
+}
+matrix[k] = NULL;
+return (matrix);
 }
